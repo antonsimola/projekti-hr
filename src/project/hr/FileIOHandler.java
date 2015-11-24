742bbb28b204541fc3ffcd9c6c85c5dc9d691681
nuKle0ni <anton.simola@lut.fi>
2015-11-24 11:40:41 +0200

    PDF libraries added

--- src/project/hr/FileIOHandler.java
+++ src/project/hr/FileIOHandler.java
@@ -21,6 +21,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 /**
  *
  * @author Samuli
@@ -33,6 +40,7 @@ public class FileIOHandler {
     private FileIOHandler() {
        userLogFilename = "HR_user_log.txt";
        dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+       writePDF();
     }
     
     public static FileIOHandler getInstance() {
@@ -82,5 +90,33 @@ public class FileIOHandler {
         }
         
         return lineList;
+    }
+    
+    public void writePDF() {
+                Document document = new Document();
+
+        try {
+            PdfWriter.getInstance(document,
+                new FileOutputStream("HelloWorld-Table.pdf"));
+
+            document.open();
+
+            PdfPTable table = new PdfPTable(3); // 3 columns.
+
+            PdfPCell cell1 = new PdfPCell(new Paragraph("Cell 1"));
+            PdfPCell cell2 = new PdfPCell(new Paragraph("Cell 2"));
+            PdfPCell cell3 = new PdfPCell(new Paragraph("Cell 3"));
+
+            table.addCell(cell1);
+            table.addCell(cell2);
+            table.addCell(cell3);
+
+            document.add(table);
+
+            document.close();
+        } catch(Exception e){
+
+        }
+        
     }
 }
